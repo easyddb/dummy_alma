f6f167f171875cf1ee56e09e5b24f5246e05924d
amdk8 <amd_k8@mail.ru>
2014-09-11 15:20:33 +0300

    DDBTEST 77 - Corrected reset script.

--- web/reset.php
+++ web/reset.php
@@ -8,9 +8,7 @@ $dir = dirname(__FILE__);
 
 $commands = array(
   "cd $dir",
-  "mysqladmin -u root -p'$db_pass' drop $db_name",
-  "mysqladmin -u root -p'$db_pass' create $db_name",
-  "mysql -u root -p'$db_pass' $db_name < $dir/src/Provider/AlmaBundle/dummy_alma.sql"
+  "mysql -u root -p'$db_pass' $db_name < $dir/../src/Provider/AlmaBundle/dummy_alma.sql"
 );
 
 $cmd = implode(' && ', $commands);
